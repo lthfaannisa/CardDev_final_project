59658365248103ed2ed34bf7689b975980b834e3
Kelsy Amirah <kelsy@mhs.unsyiah.ac.id>
2023-05-10 05:53:34 +0800

    feat: add icon as website logo

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <!-- Favicon icon -->
+    <link rel="icon" href="https://simkuliah.usk.ac.id/assets/unsyiah.png" type="image/x-icon">
+    <script src="https://cdn.tailwindcss.com"></script>
     <title> SIMBA </title>
     @vite('resources/css/app.css')
 </head>
