b33d824aada9d895d063207b698506e54612a9e6
Laila Asrin <laila_as@mhs.unsyiah.ac.id>
2023-05-15 06:08:24 +0700

    docs: modify navbar

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -31,7 +31,7 @@
                         <a href="./pages/sign-in.html"
                             class="block px-0 py-2 font-semibold transition-all ease-nav-brand text-sm text-slate-500">
                             <i class="fa fa-user sm:mr-1"></i>
-                            <span class="hidden sm:inline">Sign In</span>
+                            <span class="hidden sm:inline">{{ Auth::user()->name }}</span>
                         </a>
                     </li>
                     <li class="flex items-center pl-4 xl:hidden">
