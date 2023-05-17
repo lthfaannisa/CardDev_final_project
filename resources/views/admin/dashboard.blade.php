755744ba213803bfcae127dda7fe00962cdac6aa
Nuzulurrahmah <nuzulurrahmah3899@gmail.com>
2023-05-17 09:08:50 +0700

    docs: modify card in dashboard page

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -192,6 +192,142 @@
         });
     </script>
 
+    
+
+    <!-- cards row 4 -->
+
+    <div class="flex flex-wrap mt-4 -mx-3 px-3">
+        <div class="w-full mt-0 mb-1 lg:mb-0 lg:w-full lg:flex-none">
+            <div class="border-black/12.5 shadow-soft-xl relative z-20 flex min-w-0 flex-col break-words rounded-2xl border-0 border-solid bg-white bg-clip-border">
+                <div class="flex-auto px-3 md:mt-4">
+
+                    <h6 class="mb-0 ml-2 font-bold">Total Alumni</h6>
+                    <p class="ml-2 leading-normal text-sm"><span class="font-semibold">Berdasarkan Minat</span></p>
+                    <div class="w-full px-6 mx-auto max-w-screen-2xl rounded-xl">
+                        <div class="flex flex-wrap mt-0 -mx-3">
+                            <div class="flex-none w-1/4 max-w-full py-4 pl-0 pr-3 mt-0">
+                                <div class="flex mb-2">
+                                    <div class="flex items-center justify-center w-10 h-10 mr-2 text-center bg-center rounded fill-current shadow-soft-2xl" style="background: linear-gradient(to right, #A3A47F, #8FA781)">
+
+                                        <img src="image/data-mining.svg" alt="" width="18px" height="18px">
+
+                                    </div>
+                                    <p class="mt-1 mb-0 font-bold leading-tight text-sm">Data Mining</p>
+                                </div>
+                                <h4 class="font-bold">145</h4>
+                                <div class="text-xs h-0.75 flex w-3/4 overflow-visible rounded-lg bg-gray-200">
+                                    <div class="duration-600 ease-soft -mt-0.38 w-3/10 -ml-px flex h-1.5 flex-col justify-center overflow-hidden whitespace-nowrap rounded-lg bg-slate-700 text-center text-white transition-all" role="progressbar" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100">
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="flex-none w-1/4 max-w-full py-4 pl-0 pr-3 mt-0">
+                                <div class="flex mb-2">
+                                    <div class="flex items-center justify-center w-10 h-10 mr-2 text-center bg-center rounded fill-current shadow-soft-2xl" style="background: linear-gradient(to right, #A3A47F, #8FA781)">
+
+                                        <img src="image/rpl.svg" alt="" width="18px" height="18px">
+
+                                    </div>
+                                    <p class="mt-1 mb-0 font-bold leading-tight text-sm">RPL</p>
+                                </div>
+                                <h4 class="font-bold">158</h4>
+                                <div class="text-xs h-0.75 flex w-3/4 overflow-visible rounded-lg bg-gray-200">
+                                    <div class="duration-600 ease-soft -mt-0.38 w-3/10 -ml-px flex h-1.5 flex-col justify-center overflow-hidden whitespace-nowrap rounded-lg bg-slate-700 text-center text-white transition-all" role="progressbar" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100">
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="flex-none w-1/4 max-w-full py-4 pl-0 pr-3 mt-0">
+                                <div class="flex mb-2">
+                                    <div class="flex items-center justify-center w-10 h-10 mr-2 text-center bg-center rounded fill-current shadow-soft-2xl" style="background: linear-gradient(to right, #A3A47F, #8FA781)">
+
+                                        <img src="image/jaringan.svg" alt="" width="18px" height="18px">
+
+                                    </div>
+                                    <p class="mt-1 mb-0 font-bold leading-tight text-sm">Jaringan</p>
+                                </div>
+                                <h4 class="font-bold">95</h4>
+                                <div class="text-xs h-0.75 flex w-3/4 overflow-visible rounded-lg bg-gray-200">
+                                    <div class="duration-600 ease-soft -mt-0.38 w-2/10 -ml-px flex h-1.5 flex-col justify-center overflow-hidden whitespace-nowrap rounded-lg bg-slate-700 text-center text-white transition-all" role="progressbar" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100">
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="flex-none w-1/4 max-w-full py-4 pl-0 pr-3 mt-0">
+                                <div class="flex mb-2">
+                                    <div class="flex items-center justify-center w-10 h-10 mr-2 text-center bg-center rounded fill-current shadow-soft-2xl" style="background: linear-gradient(to right, #A3A47F, #8FA781)">
+
+                                        <img src="image/gis.svg" alt="" width="18px" height="18px">
+
+                                    </div>
+                                    <p class="mt-1 mb-0 font-bold leading-tight text-sm">GIS</p>
+                                </div>
+                                <h4 class="font-bold">89</h4>
+                                <div class="text-xs h-0.75 flex w-3/4 overflow-visible rounded-lg bg-gray-200">
+                                    <div class="duration-400 ease-soft -mt-0.38 w-1/10 -ml-px flex h-1.5 flex-col justify-center overflow-hidden whitespace-nowrap rounded-lg bg-slate-700 text-center text-white transition-all" role="progressbar" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100">
+                                    </div>
+                                </div>
+                            </div>
+
+                        </div>
+
+                    </div>
+                </div>
+                <div class="md:mt-4 mb-4 mx-4">
+                    <canvas id="chart-bars" height="80"></canvas>
+                    <script>
+                        var ctx = document.getElementById('chart-bars').getContext('2d');
+                        var chart = new Chart(ctx, {
+                            type: 'bar',
+                            data: {
+                                labels: ['Data Mining', 'RPL', 'Jaringan', 'GIS'],
+                                datasets: [{
+                                    label: 'Jumlah Alumni',
+                                    data: [145, 158, 95, 89],
+                                    backgroundColor: '#A3A47F',
+                                    borderColor: '#A3A47F',
+                                    borderWidth: 1
+                                }]
+                            },
+                            options: {
+                                scales: {
+                                    y: {
+                                        beginAtZero: true
+                                    }
+                                }
+                            }
+                        });
+                    </script>
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+    <!-- cards row 5 -->
+
+    <div class="flex flex-wrap my-6 -mx-3">
+
+
+    </div>
+
+    <footer>
+        <div class="w-full px-6 mx-auto">
+            <div class="flex flex-wrap items-center -mx-3 lg:justify-between">
+                <div class="w-full max-w-full px-3 mt-0 mb-6 shrink-0 lg:mb-0 lg:w-1/2 lg:flex-none">
+                    <div class="leading-normal text-center text-sm text-slate-500 lg:text-left">
+                        Copyright © CardDev Team 2023
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </footer>
+
+
+</div>
+<!-- end cards -->
+
 
 
 
