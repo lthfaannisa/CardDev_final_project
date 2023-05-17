0d31b350db9d5d2c2585849b1e5835ea2f8c2477
Laila Asrin <laila_as@mhs.unsyiah.ac.id>
2023-05-18 00:36:24 +0700

    docs : modify pagescontroller

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -1,6 +1,7 @@
 <?php
 namespace App\Http\Controllers;
 
+use App\Models\Student;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use RealRashid\SweetAlert\Facades\Alert;
@@ -19,17 +20,25 @@ class PagesController extends Controller
     {
 
         $role = Auth::user()->role;
+        $students = Student::all();
 
         if ($role == '1') {
-            return view('admin.dashboard', [
-                'title' => 'Dashboard',
-            ]);
+            $categories1 = Student::where('bidang_minat', 'Data Mining & Kecerdasan Artfisial')->count();
+            $categories2 = Student::where('bidang_minat', 'Rekayasa Perangkat Lunak')->count();
+            $categories3 = Student::where('bidang_minat', 'Jaringan Komputer & Keamanan Siber')->count();
+            $categories4 = Student::where('bidang_minat', 'GeoSpasial')->count();
+            $categories5 = Student::where('tahun_lulus', '2020')->count();
+            $categories6 = Student::where('tahun_lulus', '2021')->count();
+            $categories7 = Student::where('tahun_lulus', '2022')->count();
+            return view('admin.dashboard')->with('students', $students, )->with('categories1', $categories1)->with('categories2', $categories2)->with('categories3', $categories3)->with('categories4', $categories4)->with('categories5', $categories5)->with('categories6', $categories6)->with('categories7', $categories7);
         }
 
         if ($role == '2') {
-            return view('user.dashboard', [
-                'title' => 'Dashboard',
-            ]);
+            $categories1 = Student::where('bidang_minat', 'Data Mining & Kecerdasan Artfisial')->count();
+            $categories2 = Student::where('bidang_minat', 'Rekayasa Perangkat Lunak')->count();
+            $categories3 = Student::where('bidang_minat', 'Jaringan Komputer & Keamanan Siber')->count();
+            $categories4 = Student::where('bidang_minat', 'GeoSpasial')->count();
+            return view('user.dashboard')->with('students', $students, )->with('categories1', $categories1)->with('categories2', $categories2)->with('categories3', $categories3)->with('categories4', $categories4);
         }
     }
 
