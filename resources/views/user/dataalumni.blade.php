b32cafa8e0dde129e6e8cfcac1aa50d7fea324db
Kelsy Amirah <kelsy@mhs.unsyiah.ac.id>
2023-05-18 02:55:06 +0800

    docs: modify page dataalumni

--- resources/views/user/dataalumni.blade.php
+++ resources/views/user/dataalumni.blade.php
@@ -9,7 +9,7 @@
     <!-- Favicon icon -->
     <link rel="icon" href="https://simkuliah.unsyiah.ac.id/assets/unsyiah.png" type="image/x-icon">
     <title>SIMBA</title>
-    <!--     Fonts and icons     -->
+    <!-- Fonts and icons -->
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet" />
     <!-- Font Awesome Icons -->
     <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
@@ -111,84 +111,59 @@
     </aside>
     <!-- end sidenav -->
 
-    <!-- content -->
-        <!-- Navbar -->
-
-        <!-- Navbar -->
-<main class="ease-soft-in-out xl:ml-68.5 relative h-full max-h-screen rounded-xl transition-all duration-200">
     <!-- Navbar -->
-    <nav class="relative flex flex-wrap items-center justify-between px-0 py-2 mx-6 transition-all shadow-none duration-250 ease-soft-in rounded-2xl lg:flex-nowrap lg:justify-start" navbar-main navbar-scroll="true">
-      <div class="flex items-center justify-between w-full px-4 py-1 mx-auto flex-wrap-inherit">
-        <nav>
-          <!-- breadcrumb -->
-          <ol class="flex flex-wrap pt-1 mr-12 bg-transparent rounded-lg sm:mr-16">
-            <li class="leading-normal text-sm">
-              <a class="opacity-50 text-slate-700" href="javascript:;">Pages</a>
-            </li>
-            <li class="text-sm pl-2 capitalize leading-normal text-slate-700 before:float-left before:pr-2 before:text-gray-600 before:content-['/']" aria-current="page">Data Alumni</li>
-          </ol>
-          <h6 class="mb-0 font-bold capitalize">Data Alumni</h6>
+    <main class="ease-soft-in-out xl:ml-68.5 relative h-full max-h-screen rounded-xl transition-all duration-200">
+        <!-- Navbar -->
+        <nav class="relative flex flex-wrap items-center justify-between px-0 py-2 mx-6 transition-all shadow-none duration-250 ease-soft-in rounded-2xl lg:flex-nowrap lg:justify-start" navbar-main navbar-scroll="true">
+            <div class="flex items-center justify-between w-full px-4 py-1 mx-auto flex-wrap-inherit">
+                <nav>
+                    <!-- breadcrumb -->
+                    <ol class="flex flex-wrap pt-1 mr-12 bg-transparent rounded-lg sm:mr-16">
+                        <li class="leading-normal text-sm">
+                            <a class="opacity-50 text-slate-700" href="javascript:;">Pages</a>
+                        </li>
+                        <li class="text-sm pl-2 capitalize leading-normal text-slate-700 before:float-left before:pr-2 before:text-gray-600 before:content-['/']" aria-current="page">Data Alumni</li>
+                    </ol>
+                    <h6 class="mb-0 font-bold capitalize">Data Alumni</h6>
+                </nav>
+                <div class="flex items-center mt-2 grow sm:mt-0 sm:mr-6 md:mr-0 lg:flex lg:basis-auto">
+                    <div class="flex items-center md:ml-auto md:pr-4">
+                    </div>
+                    <ul class="flex flex-row justify-end pl-0 mb-0 list-none md-max:w-full">
+                        <li class="flex items-center">
+                            <a class="block px-0 py-2 font-semibold transition-all ease-nav-brand text-sm text-slate-500">
+                                <i class="fa fa-user sm:mr-1"></i>
+                                <span class="hidden sm:inline">{{ Auth::user()->name }}</span>
+                            </a>
+                        </li>
+                        <li class="flex items-center pl-4 xl:hidden">
+                            <a href="javascript:;" class="block p-0 transition-all ease-nav-brand text-sm text-slate-500" sidenav-trigger>
+                                <div class="w-4.5 overflow-hidden">
+                                    <i class="ease-soft mb-0.75 relative block h-0.5 rounded-sm bg-slate-500 transition-all"></i>
+                                    <i class="ease-soft mb-0.75 relative block h-0.5 rounded-sm bg-slate-500 transition-all"></i>
+                                    <i class="ease-soft relative block h-0.5 rounded-sm bg-slate-500 transition-all"></i>
+                                </div>
+                            </a>
+                        </li>
+                </div>
+            </div>
         </nav>
 
-        <div class="flex items-center mt-2 grow sm:mt-0 sm:mr-6 md:mr-0 lg:flex lg:basis-auto">
-          <div class="flex items-center md:ml-auto md:pr-4">
-          </div>
-          <ul class="flex flex-row justify-end pl-0 mb-0 list-none md-max:w-full">
-            <!-- online builder btn  -->
-            <!-- <li class="flex items-center">
-              <a class="inline-block px-8 py-2 mb-0 mr-4 font-bold text-center uppercase align-middle transition-all bg-transparent border border-solid rounded-lg shadow-none cursor-pointer leading-pro border-fuchsia-500 ease-soft-in text-xs hover:scale-102 active:shadow-soft-xs text-fuchsia-500 hover:border-fuchsia-500 active:bg-fuchsia-500 active:hover:text-fuchsia-500 hover:text-fuchsia-500 tracking-tight-soft hover:bg-transparent hover:opacity-75 hover:shadow-none active:text-white active:hover:bg-transparent" target="_blank" href="https://www.creative-tim.com/builder/soft-ui?ref=navbar-dashboard&amp;_ga=2.76518741.1192788655.1647724933-1242940210.1644448053">Online Builder</a>
-            </li> -->
-            <li class="flex items-center">
-              <a class="block px-0 py-2 font-semibold transition-all ease-nav-brand text-sm text-slate-500">
-                <i class="fa fa-user sm:mr-1"></i>
-                <span class="hidden sm:inline">{{ Auth::user()->name }}</span>
-              </a>
-            </li>
-            <li class="flex items-center pl-4 xl:hidden">
-              <a href="javascript:;" class="block p-0 transition-all ease-nav-brand text-sm text-slate-500" sidenav-trigger>
-                <div class="w-4.5 overflow-hidden">
-                  <i class="ease-soft mb-0.75 relative block h-0.5 rounded-sm bg-slate-500 transition-all"></i>
-                  <i class="ease-soft mb-0.75 relative block h-0.5 rounded-sm bg-slate-500 transition-all"></i>
-                  <i class="ease-soft relative block h-0.5 rounded-sm bg-slate-500 transition-all"></i>
-                </div>
-              </a>
-            </li>
-                    </div>
-                  </a>
-                </li>
-              </ul>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
-
-        <!-- table 1 -->
-
-        <div class="w-full px-8 py-8 mx-auto mt-6 ml-2 ms-2">
-            <div class="flex flex-wrap -mx-1">
+        <!-- Table 1 -->
+        <div class="w-full px-8 py-8 mx-auto mt-6">
+            <div class="flex flex-wrap -mx-3">
                 <div class="flex-none w-full max-w-full px-3">
-                    <form action="search" method="GET">
-                        <div class="flex items-center md:ml-auto md:pr-4">
+                    <div class="relative flex flex-col min-w-0 mb-6 break-words bg-white border-0 border-transparent border-solid shadow-soft-xl rounded-2xl bg-clip-border">
+                        <div class="flex items-center md:ml-auto md:pr-4 mt-2">
                             <div class="relative flex flex-wrap items-stretch w-full transition-all rounded-lg ease-soft">
                                 <span class="text-sm ease-soft leading-5.6 absolute z-50 -ml-px flex h-full items-center whitespace-nowrap rounded-lg rounded-tr-none rounded-br-none border border-r-0 border-transparent bg-transparent py-2 px-2.5 text-center font-normal text-slate-500 transition-all">
                                     <i class="fas fa-search" aria-hidden="true"></i>
                                 </span>
                                 <input type="search" name="keyword" class="pl-8.75 text-sm focus:shadow-soft-primary-outline ease-soft w-1/100 leading-5.6 relative -ml-px block min-w-0 flex-auto rounded-lg border border-solid border-gray-300 bg-white bg-clip-padding py-2 pr-3 text-gray-700 transition-all placeholder:text-gray-500 focus:border-fuchsia-300 focus:outline-none focus:transition-shadow" placeholder="Type here..." />
                             </div>
-                </form>
-                </div>
-            </div>
-        </div>
-
-        <div class="w-full px-8 py-8 mx-auto mt-6">
-            <div class="flex flex-wrap -mx-3">
-                <div class="flex-none w-full max-w-full px-3">       
-                    <div class="relative flex flex-col min-w-0 mb-6 break-words bg-white border-0 border-transparent border-solid shadow-soft-xl rounded-2xl bg-clip-border">
+                        </div>
                         <div class="flex items-center justify-between p-6 pb-0 mb-0 bg-white border-b-0 border-b-solid rounded-t-2xl border-b-transparent">
-                            
                             <h6 class="mr-auto">Data Alumni</h6>
-
                         </div>
                         <div class="flex-auto px-2 pt-0 pb-2 pl-4">
                             <div class="p-2 overflow-x-auto">
@@ -227,7 +202,6 @@
                                                 <a href="{{ url('/show', $item->id) }}" title="View Student"><button class="btn btn-info btn-sm"><i class="fa fa-eye" aria-hidden="true"></i>
                                                         View</button></a>
                                             </td>
-
                                         </tr>
                                         @endforeach
                                     </tbody>
@@ -249,7 +223,6 @@
                     </div>
                 </div>
             </footer>
-
             <!--Footer End-->
         </div>
     </main>
