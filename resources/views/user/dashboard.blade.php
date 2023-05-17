7fddd30dee8d38515d5414730ba4332be2ecc4b4
Nuzulurrahmah <nuzulurrahmah3899@gmail.com>
2023-05-17 19:09:37 +0700

    docs: delete unused footer

--- resources/views/user/dashboard.blade.php
+++ resources/views/user/dashboard.blade.php
@@ -323,28 +323,7 @@
             </div>
 
         </div>
-
-        <!-- cards row 4 -->
-
-        <div class="flex flex-wrap my-6 -mx-3">
-
-
         </div>
-
-        <footer>
-            <div class="w-full px-6 mx-auto">
-                <div class="flex flex-wrap items-center -mx-3 lg:justify-between">
-                    <div class="w-full max-w-full px-3 mt-0 mb-6 shrink-0 lg:mb-0 lg:w-1/2 lg:flex-none">
-                        <div class="leading-normal text-center text-sm text-slate-500 lg:text-left">
-                            Copyright © CardDev Team 2023
-                        </div>
-                    </div>
-
-                </div>
-            </div>
-        </footer>
-
-
     </div>
     <!-- end cards -->
 @endsection
